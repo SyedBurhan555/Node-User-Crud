5ec89a2e979b8d43fc4c8a6ad0df78480fa4cf68
Syed Burhan <syedburhanali555@gmail.com>
2023-05-24 17:20:43 +0500

    feat/add-some-middlewear

--- index.js
+++ index.js
@@ -2,11 +2,28 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8000;
 const dotenv = require("dotenv");
+const path = require("path");
+const session = require("express-session");
 
 dotenv.config({ path: "./config.env" });
 require("./db/conn");
 
+//middlewears
+
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  session({
+    secret: "my serect Key",
+    saveUninitialized: true,
+    resave: false,
+  })
+);
+app.use((req, res, next) => {
+  res.locals.message = req.session.message;
+  delete req.session.message;
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("hello node js");
